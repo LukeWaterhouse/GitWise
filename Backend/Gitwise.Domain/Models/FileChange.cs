fbdff208c64c7fcbe9f712345f365c45788497af
luke.waterhouse <lukewwaterhouse@hotmail.co.uk>
2025-10-23 21:41:31 +0100

    Update FileChange.cs

--- Backend/Gitwise.Domain/Models/FileChange.cs
+++ Backend/Gitwise.Domain/Models/FileChange.cs
@@ -1,7 +1,15 @@
 namespace Gitwise.Domain.Models;
 
-public record FileChange(
-    string FileSnapshotSha,
-    string FileName,
-    ChangeStats ChangeStats,
-    string ChangeDefinition);
+public class FileChange(
+    string fileSnapshotSha,
+    string fileName,
+    ChangeStats changeStats,
+    string changeDefinition,
+    FileSnapshot? fileSnapshot)
+{
+    public string FileSnapshotSha { get; } = fileSnapshotSha;
+    public string FileName { get; } = fileName;
+    public ChangeStats ChangeStats { get; } = changeStats;
+    public string ChangeDefinition { get; } = changeDefinition;
+    public FileSnapshot? FileSnapshot { get; set; } = fileSnapshot;
+}
